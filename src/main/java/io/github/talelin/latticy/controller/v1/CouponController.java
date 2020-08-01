0c5ef2310c5af30e3ca1ac67da3919f9881b0840
liyang <xxbarca@163.com>
2020-08-01 16:58:47 +0800

    获取优惠券

--- src/main/java/io/github/talelin/latticy/controller/v1/CouponController.java
+++ src/main/java/io/github/talelin/latticy/controller/v1/CouponController.java
@@ -1,9 +1,12 @@
 package io.github.talelin.latticy.controller.v1;
 
+import io.github.talelin.autoconfigure.exception.NotFoundException;
 import io.github.talelin.core.annotation.GroupRequired;
+import io.github.talelin.core.annotation.LoginRequired;
 import io.github.talelin.core.annotation.PermissionMeta;
 import io.github.talelin.core.annotation.PermissionModule;
 import io.github.talelin.latticy.dto.CouponDTO;
+import io.github.talelin.latticy.model.CouponDO;
 import io.github.talelin.latticy.service.CouponService;
 import io.github.talelin.latticy.vo.CreatedVO;
 import io.github.talelin.latticy.vo.DeletedVO;
@@ -36,5 +39,15 @@ public class CouponController {
     public DeletedVO delete(@PathVariable @Positive(message = "{id}") Integer id) {
         this.couponService.delete(id);
         return new DeletedVO();
+    }
+
+    @GetMapping("/{id}")
+    @LoginRequired
+    public CouponDO get(@PathVariable @Positive(message = "{id}") Integer id) {
+        CouponDO couponDO = this.couponService.getBaseMapper().selectById(id);
+        if (couponDO == null) {
+            throw new NotFoundException(100000);
+        }
+        return couponDO;
     }
 }

3aedc3068ba2993a8bf6de1966a2f565ab0b06fd
liyang <xxbarca@163.com>
2020-10-29 20:21:40 +0800

    获取theme详情

--- src/main/java/io/github/talelin/latticy/controller/v1/ThemeController.java
+++ src/main/java/io/github/talelin/latticy/controller/v1/ThemeController.java
@@ -1,6 +1,7 @@
 package io.github.talelin.latticy.controller.v1;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import io.github.talelin.autoconfigure.exception.NotFoundException;
 import io.github.talelin.core.annotation.LoginRequired;
 import io.github.talelin.latticy.common.mybatis.Page;
 import io.github.talelin.latticy.common.util.PageUtil;
@@ -8,13 +9,11 @@ import io.github.talelin.latticy.model.ThemeDO;
 import io.github.talelin.latticy.service.ThemeService;
 import io.github.talelin.latticy.vo.PageResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("/v1/theme")
@@ -35,5 +34,15 @@ public class ThemeController {
         Page<ThemeDO> pager = new Page<>(page, count);
         IPage<ThemeDO> paging = themeService.getBaseMapper().selectPage(pager, null);
         return PageUtil.build(paging);
+    }
+
+    @GetMapping("/{id}")
+    @LoginRequired
+    public ThemeDO get(@PathVariable(value = "id") @Positive(message = "{id.positive}") Integer id) {
+        ThemeDO theme = themeService.getBaseMapper().selectById(id);
+        if (theme == null) {
+            throw new NotFoundException(30000);
+        }
+        return theme;
     }
 }

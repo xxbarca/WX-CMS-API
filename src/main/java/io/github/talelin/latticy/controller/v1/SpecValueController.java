cc7a3dd3116f3e23fe8c7382e4681b7e7822f068
liyang <xxbarca@163.com>
2020-10-30 13:36:08 +0800

    根据规格名获取规格值

--- src/main/java/io/github/talelin/latticy/controller/v1/SpecValueController.java
+++ src/main/java/io/github/talelin/latticy/controller/v1/SpecValueController.java
@@ -1,6 +1,8 @@
 package io.github.talelin.latticy.controller.v1;
 
 
+import io.github.talelin.latticy.service.impl.SpecValueServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +22,8 @@ import javax.validation.constraints.Positive;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
 * @author generator@TaleLin
 * @since 2020-05-29
@@ -28,35 +32,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/spec-value")
 public class SpecValueController {
 
-    @PostMapping("")
-    public CreatedVO create() {
-        return new CreatedVO();
-    }
+    @Autowired
+    private SpecValueServiceImpl specValueService;
 
-    @PutMapping("/{id}")
-    public UpdatedVO update(@PathVariable @Positive(message = "{id.positive}") Long id) {
-        return new UpdatedVO();
-    }
-
-    @DeleteMapping("/{id}")
-    public DeletedVO delete(@PathVariable @Positive(message = "{id.positive}") Long id) {
-        return new DeletedVO();
-    }
-
-    @GetMapping("/{id}")
-    public SpecValueDO get(@PathVariable(value = "id") @Positive(message = "{id.positive}") Long id) {
-        return null;
-    }
-
-    @GetMapping("/page")
-    public PageResponseVO<SpecValueDO> page(
-            @RequestParam(name = "count", required = false, defaultValue = "10")
-            @Min(value = 1, message = "{page.count.min}")
-            @Max(value = 30, message = "{page.count.max}") Long count,
-            @RequestParam(name = "page", required = false, defaultValue = "0")
-            @Min(value = 0, message = "{page.number.min}") Long page
-    ) {
-        return null;
+    @GetMapping("/by/spec-key/{id}")
+    public List<SpecValueDO> getBySpecKeyId(@PathVariable(value = "id") @Positive(message = "{id.positive}") Integer id) {
+        return this.specValueService.lambdaQuery().eq(SpecValueDO::getSpecId, id).list();
     }
 
 }
